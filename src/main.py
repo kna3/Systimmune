783d02cecde79d54a3b44f31ea137e11bb67d13c
Karan LNU/m <kna3@binghamton.edu>
2020-08-12 01:25:06 -0400

    Creating the Tables SalesPerson, Branch and CarSold in CarDealership Database

--- src/main.py
+++ src/main.py
@@ -16,3 +16,12 @@ mycursor.execute("CREATE TABLE IF NOT EXISTS Vehicle (vehicle_id INT(20) PRIMARY
 
 #Creating the Table Inventory in CarDealership Database
 mycursor.execute("CREATE TABLE IF NOT EXISTS Inventory (inventory_id INT(20) NOT NULL,vehicle_id INT(30) NOT NULL,is_sold boolean default false,vehicle_state ENUM('New','Used') NOT NULL,inventory_description VARCHAR(50),inventory_title ENUM('Clean','Rebuilt','Salvage'),branch_location VARCHAR(30) NOT NULL)")
+
+#Creating the Table SalesPerson in CarDealership Database
+mycursor.execute("CREATE TABLE IF NOT EXISTS SalesPerson (salesperson_id INT(20) NOT NULL,salesperson_name VARCHAR(30),salesperson_phone INT(10),branch_location VARCHAR(30))")
+
+#Creating the Table Branch in CarDealership Database
+mycursor.execute("CREATE TABLE IF NOT EXISTS Branch (branch_location VARCHAR(30),branch_code INT(20))")
+
+#Creating the Table CarSold in CarDealership Database
+mycursor.execute("CREATE TABLE IF NOT EXISTS CarSold (sale_id INT(30),vehicle_id INT(20), salesperson_id INT(20),customer_id INT(20),date_sold DATE NOT NULL)")
