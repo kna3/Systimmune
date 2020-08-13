32aff2a17ad45b28d5d1cb2b15980d0a16770b6f
Karan LNU/m <kna3@binghamton.edu>
2020-08-13 18:13:03 -0400

    Final Commit

--- src/app.py
+++ src/app.py
@@ -16,7 +16,7 @@ parser.add_argument('-f2', required=True)
 parser.add_argument('-f3', required=True)
 parser.add_argument('-f4', required=True)
 parser.add_argument('-f5', required=True)
-
+parser.add_argument('-f6', required=True)
 args=vars(parser.parse_args())
 
 file1=args["f1"]
@@ -24,10 +24,11 @@ file2=args["f2"]
 file3=args["f3"]
 file4=args["f4"]
 file5=args["f5"]
+file6=args["f6"]
 
 #Invoke function to setup database and add data to tables
 def setupDatabase():
-    setup.create(file1,file2,file3,file4,file5)
+    setup.create(file1,file2,file3,file4,file5,file6)
     app.run()
 
 #Method to render index.html 
@@ -40,16 +41,16 @@ def hello():
 def sendDate():
     startDate = request.form.get('start_date')
     endDate = request.form.get('end_date')
-    data = setup.getBestSalesperson(startDate, endDate)
-    data2= setup.getTopModel(startDate, endDate)
+    bestSalesPerson = setup.getBestSalesperson(startDate, endDate)
+    topModel= setup.getTopModel(startDate, endDate)
+    noSalesPerson = None
+    noTopModel = None
+    if not bestSalesPerson:
+        noSalesPerson = True
+    if not topModel:
+        noTopModel = True
 
-    return render_template("index.html", salesmanName=data, modelName = data2)
-
-@app.route('/response', methods=['POST'])
-def response():
-    fname = request.form.get("fname")
-    note = request.form.get("note")
-    return render_template("index.html", name=fname, note=note)
+    return render_template("index.html", salesmanName=bestSalesPerson, modelName = topModel, hasBestSalesPerson = noSalesPerson, hasTopModel = noTopModel)
 
 if __name__ == '__main__':
     setupDatabase()
